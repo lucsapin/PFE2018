af13cd5f1accc1f80089bf2d1a752fd2fd61a17b
lucsapin <luc.sapin@gmail.com>
2018-07-17 17:59:03 +0200

    try new opti on 5 phasis transfert

--- simulations_asteroides/bocop/exec_bocop_3p_continuous_Hill.m
+++ simulations_asteroides/bocop/exec_bocop_3p_continuous_Hill.m
@@ -1,6 +1,6 @@
 function [time,stage,z,control,optimvars,output] = exec_bocop_3B_impulse_hill(bocop_def_pb, init, par, options, solFileName)
 
-workspace = [bocop_def_pb 'def_pb_duree_min_5p/'];
+workspace = [bocop_def_pb 'def_pb_duree_min_3p/'];
 % -----------------------------------
 % Bocop options
 % -----------------------------------
@@ -19,24 +19,21 @@ options_ipopt = [{'max_iter','1000' };
 % -----------------------------------
 % Dimensions and names
 % -----------------------------------
-dims.state          = 35;
-dims.control        = 9;
+dims.state          = 21;
+dims.control        = 6;
 dims.algebraic      = 0;
-dims.parameter      = 5;
+dims.parameter      = 3;
 dims.constant       = 20;
-dims.boundarycond   = 41;
-dims.constraint     = 3;
+dims.boundarycond   = 27;
+dims.constraint     = 2;
 
 %
 name_state      = {'q11','q12','q13','q14','q15','q16','m1', ...
                    'q21','q22','q23','q24','q25','q26','m2', ...
-                   'q31','q32','q33','q34','q35','q36','m3', ...
-                   'q41','q42','q43','q44','q45','q46','m4', ...
-                   'q51','q52','q53','q54','q55','q56','m5'};
+                   'q31','q32','q33','q34','q35','q36','m3'};
 
 name_control    = {'u11','u12','u13', ...
-                   'u31','u32','u33', ...
-                   'u51','u52','u53'};
+                   'u31','u32','u33'};
 
 name_boundary   = {'q11i-qH1','q12i-qH2','q13i-qH3', ...
                    'q14i-qH4','q15i-qH5','q16i-qH6','m1i-m0', ...
@@ -44,19 +41,15 @@ name_boundary   = {'q11i-qH1','q12i-qH2','q13i-qH3', ...
                    'q24i-q14f','q25i-q15f','q26i-q16f','m2i-m1f', ...
                    'q31i-q21f','q32i-q22f','q33i-q23f', ...
                    'q34i-q24f','q35i-q25f','q36i-q26f','m3i-m2f', ...
-                   'q41i-q31f','q42i-q32f','q43i-q33f', ...
-                   'q44f-q34f','q45i-q35f','q46i-q36f','m4i-m3f', ...
-                   'q51i-q41f','q52i-q42f','q53i-q43f', ...
-                   'q54i-q44f','q55i-q45f','q56i-q46f','m5i-m4f', ...
-                   'qL21-q51f','qL22-q52f','qL23-q53f', ...
-                   'qL24-q54f','qL25-q55f','qL26-q56f'}; %m5f libre
+                   'qL21-q31f','qL22-q32f','qL23-q33f', ...
+                   'qL24-q34f','qL25-q35f','qL26-q36f'}; %m3f libre
 
 name_par        = {'Tmax','beta','mu','muS','rS', ...
                    'qH1','qH2','qH3','qH4','qH5','qH6', ...
                    'qL21','qL22','qL23','qL24','qL25','qL26', ...
                    'thetaS0','omegaS','m0'};
 
-name_opti       = {'dt1','dt2','dt3','dt4','dtf'};
+name_opti       = {'dt1','dt2','dtf'};
 
 namesDef        = NamesDefFile(name_state,name_control,name_par,name_boundary,name_opti);
 
@@ -65,8 +58,6 @@ namesDef        = NamesDefFile(name_state,name_control,name_par,name_boundary,na
 % -----------------------------------
 % Bounds for the initial and final conditions :
 bifc = [0 0; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0; ...
-        0 0; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0; ...
-        0 0; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0; ...
         0 0; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0; ...
         0 0; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0; ...
         0 0; 0 0; 0 0; 0 0; 0 0; 0 0];
@@ -80,18 +71,11 @@ bsv = [-2e20 2e20; -2e20 2e20; -2e20 2e20;
        -2e20 2e20; % 2è phase
        -2e20 2e20; -2e20 2e20; -2e20 2e20;
        -2e20 2e20; -2e20 2e20; -2e20 2e20;
-       -2e20 2e20; % 3è phase
-       -2e20 2e20; -2e20 2e20; -2e20 2e20;
-       -2e20 2e20; -2e20 2e20; -2e20 2e20;
-       -2e20 2e20; % 4è phase
-       -2e20 2e20; -2e20 2e20; -2e20 2e20;
-       -2e20 2e20; -2e20 2e20; -2e20 2e20;
-       -2e20 2e20]; % 5è phase
+       -2e20 2e20]; % 3è phase
 
 % Bounds for the control variables :
 bcv = [-1 1; -1 1; -1 1; % u1
-       -1 1; -1 1; -1 1; % u3
-       -1 1; -1 1; -1 1]; % u5
+       -1 1; -1 1; -1 1]; % u3
 
 % Bounds for the algebraic variables :
 bav = [];
@@ -99,13 +83,10 @@ bav = [];
 % Bounds for the optimization parameters :
 bop = [0     2e20;
        0     2e20;
-       0     2e20;
-       0     2e20;
        0     2e20];
 
 % Bounds for the path constraints :
 bpc = [0 0;
-       0 0;
        0 0];
 
 % Bounds
@@ -131,7 +112,7 @@ zMultiphase       = [state ; -[-boundarycond_mult(1:length(state(:,1))) adjoint]
 z = [zMultiphase(1:7,:)   zMultiphase(8:14,:) zMultiphase(15:21,:) zMultiphase(22:28,:) zMultiphase(29:35,:);% state
      zMultiphase(36:42,:) zMultiphase(43:49,:) zMultiphase(50:56,:) zMultiphase(57:63,:) zMultiphase(64:70,:)]; % adjoint
 
-if ~isempty(control)
+if control ~= []
   control = [control control(:,end)];
 end
 
--- simulations_asteroides/main_phase_parking_Bocop.m
+++ simulations_asteroides/main_phase_parking_Bocop.m
@@ -231,7 +231,7 @@ end
 
 
 % ------------------------------------------------------------------------------
-% Bocop : min sum duree poussee continue
+% Bocop : min sum duree poussee continue 5p
 % parameters
 par_bocop = [Tmax; beta; muCR3BP; muSun; rhoSun; q0; qf; thetaS0; omegaS; m0];
 
@@ -255,11 +255,44 @@ map_indices_par_bocop = containers.Map(keySet, valueSet);
 
 % Initialization
 defPbBocop  = 'bocop/'; % Directory where main bocop pb directory is: ./bocop/3B_impulse_Hill/
-solFileLoad = ['../3_boosts_impulse_Hill/min_3B_impulse_bocop' ...
+solFile = ['../3_boosts_impulse_Hill/min_3B_impulse_bocop' ...
                '_Tmax_' num22str(TmaxN,3) ...
                '_m0_' num22str(m0,6) ...
                '_beta_' num22str(beta,3) ...
                '_ast_' int2str(numAsteroid) '.sol'];
+
+load(['results/compare_inside_Hill/L2/in_progress_results/min_inside_hill_bocop' ...
+      '_Tmax_' num22str(TmaxN,3) ...
+      '_m0_' num22str(m0,6) ...
+      '_beta_' num22str(beta,3) ...
+      '_ast_' int2str(numAsteroid) '.mat']);
+
+norm_dV1 = norm(results.min_3B_impulse_bocop.optimvarsB(3:5));
+norm_dV2 = norm(results.min_3B_impulse_bocop.optimvarsB(6:8));
+norm_dV3 = norm(results.min_3B_impulse_bocop.optimvarsB(9:11));
+
+init_m1 = m0;
+init_m2 = init_m1*exp(-beta*norm_dV1);
+init_m3 = init_m2;
+init_m4 = init_m3*exp(-beta*norm_dV2);
+init_m5 = init_m4;
+
+init_dt1 = init_m1*norm_dV1/Tmax;
+init_dt3 = init_m3*norm_dV2/Tmax;
+init_dtf = init_m5*norm_dV3/Tmax;
+
+init_dt2 = results.min_3B_impulse_bocop.optimvarsB(1) - (init_dt1 + init_dt3/2);
+init_dt4 = results.min_3B_impulse_bocop.optimvarsB(2) - (init_dtf + init_dt3/2);
+
+init_u11 = results.min_3B_impulse_bocop.optimvarsB(3);
+init_u12 = results.min_3B_impulse_bocop.optimvarsB(4);
+init_u13 = results.min_3B_impulse_bocop.optimvarsB(5);
+init_u31 = results.min_3B_impulse_bocop.optimvarsB(6);
+init_u32 = results.min_3B_impulse_bocop.optimvarsB(7);
+init_u33 = results.min_3B_impulse_bocop.optimvarsB(8);
+init_u51 = results.min_3B_impulse_bocop.optimvarsB(9);
+init_u52 = results.min_3B_impulse_bocop.optimvarsB(10);
+init_u53 = results.min_3B_impulse_bocop.optimvarsB(11);
 
 if(strcmp(init_choice, 'none1')==1)
 
@@ -269,9 +302,57 @@ if(strcmp(init_choice, 'none1')==1)
 
     solFileSave         = './min_5p_continuous_bocop.sol';
 
-    init.type           = 'from_sol_file_warm';
-    init.file           = solFileLoad;
-    init.X0             = [];
+    init.type           = 'from_init_file';
+    init.file           = 'none';
+    init.X0             = [{'state.0'     , 'constant', (q0(1)+qf(1))/2}; % q11
+                           {'state.1'     , 'constant', (q0(2)+qf(2))/2}; % q12
+                           {'state.2'     , 'constant', (q0(3)+qf(3))/2}; % q13
+                           {'state.3'     , 'constant', (q0(4)+qf(4))/2}; % q14
+                           {'state.4'     , 'constant', (q0(5)+qf(5))/2}; % q15
+                           {'state.5'     , 'constant', (q0(6)+qf(6))/2}; % q16
+                           {'state.6'     , 'constant', init_m1};         % m1
+                           {'state.7'     , 'constant', (q0(2)+qf(2))/2}; % q21 balistique
+                           {'state.8'     , 'constant', (q0(3)+qf(3))/2}; % q22 balistique
+                           {'state.9'     , 'constant', (q0(4)+qf(4))/2}; % q23 balistique
+                           {'state.10'    , 'constant', (q0(5)+qf(5))/2}; % q24 balistique
+                           {'state.11'    , 'constant', (q0(6)+qf(6))/2}; % q25 balistique
+                           {'state.12'    , 'constant', (q0(1)+qf(1))/2}; % q26 balistique
+                           {'state.13'    , 'constant', init_m2};         % m2  balistique
+                           {'state.14'    , 'constant', (q0(3)+qf(3))/2}; % q31
+                           {'state.15'    , 'constant', (q0(4)+qf(4))/2}; % q32
+                           {'state.16'    , 'constant', (q0(5)+qf(5))/2}; % q33
+                           {'state.17'    , 'constant', (q0(6)+qf(6))/2}; % q34
+                           {'state.18'    , 'constant', (q0(1)+qf(1))/2}; % q35
+                           {'state.19'    , 'constant', (q0(2)+qf(2))/2}; % q36
+                           {'state.20'    , 'constant', init_m3};         % m3
+                           {'state.21'    , 'constant', (q0(4)+qf(4))/2}; % q41 balistique
+                           {'state.22'    , 'constant', (q0(5)+qf(5))/2}; % q42 balistique
+                           {'state.23'    , 'constant', (q0(6)+qf(6))/2}; % q43 balistique
+                           {'state.24'    , 'constant', (q0(2)+qf(2))/2}; % q44 balistique
+                           {'state.25'    , 'constant', (q0(3)+qf(3))/2}; % q45 balistique
+                           {'state.26'    , 'constant', (q0(4)+qf(4))/2}; % q46 balistique
+                           {'state.27'    , 'constant', init_m4};         % m4  balistique
+                           {'state.28'    , 'constant', (q0(6)+qf(6))/2}; % q51
+                           {'state.29'    , 'constant', (q0(1)+qf(1))/2}; % q52
+                           {'state.30'    , 'constant', (q0(2)+qf(2))/2}; % q53
+                           {'state.31'    , 'constant', (q0(3)+qf(3))/2}; % q54
+                           {'state.32'    , 'constant', (q0(4)+qf(4))/2}; % q55
+                           {'state.33'    , 'constant', (q0(5)+qf(5))/2}; % q56
+                           {'state.34'    , 'constant', init_m5};         % m5
+                           {'optimvars.0' , 'constant', init_dt1};        % dt1
+                           {'optimvars.1' , 'constant', init_dt2};        % dt2 balistique
+                           {'optimvars.2' , 'constant', init_dt3};        % dt3
+                           {'optimvars.3' , 'constant', init_dt4};        % dt4 balistique
+                           {'optimvars.4' , 'constant', init_dtf};        % dtf
+                           {'control.0'   , 'constant', init_u11};        % u11
+                           {'control.1'   , 'constant', init_u12};        % u12
+                           {'control.2'   , 'constant', init_u13};        % u13
+                           {'control.3'   , 'constant', init_u31};        % u31
+                           {'control.4'   , 'constant', init_u32};        % u32
+                           {'control.5'   , 'constant', init_u33};        % u33
+                           {'control.6'   , 'constant', init_u51};        % u51
+                           {'control.7'   , 'constant', init_u52};        % u52
+                           {'control.8'   , 'constant', init_u53}];        % u53
 
 else
     error('No such init_choix!');
@@ -315,6 +396,119 @@ else
     uB = min_5p_continuous_bocop.uB;
     optimvarsB = min_5p_continuous_bocop.optimvarsB;
     outputB = min_5p_continuous_bocop.outputB;
+end
+
+
+% ------------------------------------------------------------------------------
+% Affichage des trajectoires
+plot = true;
+if plot
+  figure;
+  display_Moon(); hold on;
+  display_Earth(); hold on;
+  display_L2(); hold on;
+
+  plot3(Q_CR3BP(1,:), Q_CR3BP(2,:), Q_CR3BP(3,:), 'r', 'LineWidth', DC.LW); hold on;
+  plot3(zB(1,:), zB(2,:), zB(3,:), 'b', 'LineWidth', DC.LW);
+
+  % draw Hill's sphere (on the (q_1, q_2) plan)
+  viscircles([0 0], dist*UC.AU/UC.LD, 'Color', 'g');
+
+  legend('Moon', 'Earth', 'L2', '2 corps Soleil', 'Bocop');
+  title('Compare Dynamics : Compare Bocop and Propagation in Hill''s sphere');
+
+  xlabel('q_1');
+  ylabel('q_2');
+  zlabel('q_3');
+  view(0, 90);
+end
+
+
+% ------------------------------------------------------------------------------
+% Bocop : min sum duree poussee continue 3p
+% parameters
+par_bocop = [Tmax; beta; muCR3BP; muSun; rhoSun; q0; qf; thetaS0; omegaS; m0];
+
+n = 6;
+
+inc               = 1;
+iTmax             = inc;                      inc = inc + 1;
+ibeta             = inc;                      inc = inc + 1;
+imuCR3BP          = inc;                      inc = inc + 1;
+imuSun            = inc;                      inc = inc + 1;
+irhoSun           = inc;                      inc = inc + 1;
+iq0               = inc:inc+n-1;              inc = inc + n;
+iqf               = inc:inc+n-1;              inc = inc + n;
+ithetaS0          = inc;                      inc = inc + 1;
+iomegaS           = inc;                      inc = inc + 1;
+im0               = inc;                      inc = inc + 1;
+
+keySet      = {'Tmax','beta','muCR3BP','muSun','rhoSun','q0','qf','thetaS0','omegaS','m0'};
+valueSet    = {iTmax, ibeta, imuCR3BP, imuSun, irhoSun, iq0, iqf, ithetaS0, iomegaS, im0};
+map_indices_par_bocop = containers.Map(keySet, valueSet);
+
+% Initialization
+defPbBocop  = 'bocop/'; % Directory where main bocop pb directory is: ./bocop/3B_impulse_Hill/
+solFile = ['../3_boosts_impulse_Hill/min_3B_impulse_bocop' ...
+               '_Tmax_' num22str(TmaxN,3) ...
+               '_m0_' num22str(m0,6) ...
+               '_beta_' num22str(beta,3) ...
+               '_ast_' int2str(numAsteroid) '.sol'];
+
+if(strcmp(init_choice, 'none1')==1)
+
+    options             = [];
+    options.disc_steps  = '100';
+    options.disc_method = 'midpoint';
+
+    solFileSave         = './min_3p_continuous_bocop.sol';
+
+    init.type           = 'from_sol_file_warm';
+    init.file           = solFile;
+    init.X0             = [];
+
+else
+    error('No such init_choix!');
+end
+
+% Computation
+if(results.exec_min_3p_continuous_bocop==-1)
+    [toutB,stageB,zB,uB,optimvarsB,outputB] = exec_bocop_3p_continuous_Hill(defPbBocop, init, par_bocop, options, solFileSave);
+
+    if(outputB.status ~= 0)
+        error('Bocop did not converge for the minimal dV problem!');
+    end
+
+    % useful
+    min_3p_continuous_bocop.outputOpti     = outputOptimization;
+    min_3p_continuous_bocop.indices        = map_indices_par_bocop;
+
+    % inputs
+    min_3p_continuous_bocop.init           = init;
+    min_3p_continuous_bocop.par            = par_bocop;
+    min_3p_continuous_bocop.options        = options;
+
+    % outputs
+    min_3p_continuous_bocop.toutB          = toutB;
+    min_3p_continuous_bocop.stageB         = stageB;
+    min_3p_continuous_bocop.zB             = zB;
+    min_3p_continuous_bocop.uB             = uB;
+    min_3p_continuous_bocop.optimvarsB     = optimvarsB;
+    min_3p_continuous_bocop.outputB        = outputB;
+
+    % Save in progress results
+    results.exec_min_3p_continuous_bocop   = 1;
+    results.min_3p_continuous_bocop        = min_3p_continuous_bocop;
+    save(file_results, 'results');
+
+else
+    min_3p_continuous_bocop = results.min_3p_continuous_bocop;
+    toutB = min_3p_continuous_bocop.toutB;
+    stageB = min_3p_continuous_bocop.stageB;
+    zB = min_3p_continuous_bocop.zB;
+    uB = min_3p_continuous_bocop.uB;
+    optimvarsB = min_3p_continuous_bocop.optimvarsB;
+    outputB = min_3p_continuous_bocop.outputB;
 end
 
 
